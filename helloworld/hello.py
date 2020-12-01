5dd6c797d4892f79d47f7cce83c1de704f6ce4d2
trabdlkarim <trabdlkarim@programmer.net>
2020-12-02 02:00:58 +0300

    add simple python hello world program

---
+++ helloworld/hello.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""
+@author: trabdlkarim
+
+"""
+
+name = input("Quel est votre nom s'il vous plait: ")
+print("Salut, %s bienvenue à Python Boot Camp!" % name)
