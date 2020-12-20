1334c3f16086b8e8888c0772c08d093a359b3e44
Kouakou Konan Nicaise <61632596+konan08-nic@users.noreply.github.com>
2020-12-20 22:42:37 +0000

    Add files via upload

--- apps/pydevie/PyDevie.py
+++ apps/pydevie/PyDevie.py
@@ -1,25 +1,29 @@
-import random
-nbmax=10
-nbmin=1
-n= random.randint(1,100)
-t=random.randrange(1,n,7)
-reponse=''
-nbsaisie=0
-print("j'ai choisi un nombre compris entre 0 et 100, devinez-le!\n")
-print("vous avez 10 essaies! \n")
-while nbsaisie!=t and nbmin<=nbmax:
-   print("essaie N°",nbmin)
-   print("===========")
-   nbsaisie=int(input("votre devinette svp\n"))
-   if nbsaisie==t:
-       print("nombre correct")
-       
-   else:
-       print("0, le nombre n'est pas à la bonne place")       
-       nbmin=nbmin+1
-   
-   if nbmin>nbmax and t!=nbsaisie :
-             break
-   else:
-           continue 
-   print("merci d'avoir essayer!")
+
+from random import randint
+rp=1
+nbmin = 1
+nbmax = 10
+s = randint(1,100)
+v = 0
+print("soyez les bienvenus(es) dans mon jeux \n")
+print("J'ai choisi un nombre entre 1 et 100 \n")
+print("devinez en",nbmax,"tentatives au maximum !\n")
+while rp==1:
+ while v!= s and nbmin <= nbmax:
+  
+  print("Essai no ",nbmin)
+  v = int(input("Votre devinette svp:\n "))
+  if s < v:
+    print("pas juste")
+  elif s > v:
+    print("pas juste")
+  else:
+    print("Bravo ! Vous avez trouvé",v,"en",nbmin,"essai(s)")
+  nbmin=nbmin+1
+
+  if nbmin>nbmax and s!= v :
+    print("Désolé, vous avez utilisé vos",nbmax,"essais en vain.")
+    print("J'avais choisi le nombre",s,".")
+ rp=input("voulez vous recommencer? 1-oui/2-non \n")
+
+  
