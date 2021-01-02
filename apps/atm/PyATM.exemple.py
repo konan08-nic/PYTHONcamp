7d9c2dfc7d8617b82a4e1a5982670b35c1677f3c
Touré A. Karim <trabdlkarim@programmer.net>
2021-01-02 12:30:16 +0300

    Create PyATM.exemple.py

---
+++ apps/atm/PyATM.exemple.py
@@ -0,0 +1,26 @@
+#!/usr/bin/en python3
+# -*- coding: utf-8 -*-
+
+"""Un exemple pour PyATM pour aider à formater le programme
+avec la signature des differentes fonctions à définir.
+La structure de PyATM pourrait ne pas être conforme à celle-ci.
+Ceci est juste un modèle à titre indicatif. Vous pouvez définir
+votre propre structure pour PyATM à la condition que celle-ci
+respecte les exigences et spécificités de PyATM.
+
+Lire le fichier README.md situé dans le même dossier que ce 
+fichier ci pour plus d'information sur PyATM.
+"""
+
+# Définition des variables
+
+liste_des_clients = [["John","DOE","2101-0200-0100",""],
+                     [],
+                     [],
+                     [],
+                     [],
+                     [],
+                     [],
+                     [],
+                     [],
+                     []]
