e57c49221e5b96f1e9a80cec65e36246b05560d8
Touré A. Karim <trabdlkarim@programmer.net>
2021-06-21 22:26:51 +0300

    Update main.py

--- apps/ciphers/cesar/main.py
+++ apps/ciphers/cesar/main.py
@@ -1,16 +1,32 @@
-ALPHABET = []
+####################### NE RIEN CHANGER ICI ################################
+ALPHABET = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M',
+            'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
 KEY = 4
 
-# Fonction pour chiffrer les message
+# Fonction pour trouver la position des lettres de l'alphabet
+def getIndex(letter):
+    for i in range(len(ALPHABET)):
+        if uppercase(letter) == ALPHABET[i]:
+            return i
+####################### NE RIEN CHANGER ICI ################################
+
+
+# Fonction pour chiffrer les message à implémenter
 def encode(message, key):
   pass
 
-# Fonction pour dechiffer
+# Fonction pour dechiffer à implémenter
 def decode(message, key):
   pass
+
+
+
+####################### NE RIEN CHANGER ICI ################################
 
 def test(m):
     print(encode(m, KEY))
     print(decode(m, KEY))
 
 test("HELLO", KEY)
+
+####################### NE RIEN CHANGER ICI ################################
