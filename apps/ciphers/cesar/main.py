21323e75dacce644158c841ea4921dddcdfd5840
Touré A. Karim <trabdlkarim@programmer.net>
2021-06-21 22:13:13 +0300

    Update main.py

--- apps/ciphers/cesar/main.py
+++ apps/ciphers/cesar/main.py
@@ -1,1 +1,16 @@
+ALPHABET = []
+KEY = 4
 
+# Fonction pour chiffrer les message
+def encode(message, key):
+  pass
+
+# Fonction pour dechiffer
+def decode(message, key):
+  pass
+
+def test(m):
+    print(encode(m, KEY))
+    print(decode(m, KEY))
+
+test("HELLO", KEY)
